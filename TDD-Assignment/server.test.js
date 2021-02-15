21e59f7d97063358233954c446038770f78ac12c
colin kubisiak <ckubisiak@ufl.edu>
2021-02-15 17:58:25 -0500

    Tests Written

--- TDD-Assignment/server.test.js
+++ TDD-Assignment/server.test.js
@@ -1,4 +1,4 @@
-var should = require('should'), 
+var should = require('should'),
     fs = require('fs'),
     request = require('request');
 
@@ -45,8 +45,12 @@ describe('UF Directory Server Unit Tests', function() {
           In first statement, assert what we should not see
           In the second, assert what we should  see.
           Finally, call "done();" to move on to the next test.
+		  
+		  
         */
-         
+		 response.should.not.equal(null);
+		 response.should.exist;
+		 done();
       });
     });
   });
@@ -58,11 +62,15 @@ describe('UF Directory Server Unit Tests', function() {
       request.get('http://localhost:8080/listings', function(error, response, body) {
       	
       	// First let's assert that the body being passed by the get request actually exists or not with our general assertions, similar to the previous test:
-       	
+		 body.should.not.equal(null);
+		 body.should.exist;
 
 			// Next, use deepEquals() for object level comparison. We want to assert that the "listings" JSON provided by the get request is the same as the JSON file provided by the test (bodyData)
 			// Finally, call "done();" to move onto the next test
         	bodyData = JSON.parse(body);
+			bodyData.should.deepEqual(listings);
+			
+			done();
         	
       });
     });
@@ -71,12 +79,13 @@ describe('UF Directory Server Unit Tests', function() {
     it('responds with a 404 error to other GET requests', function(done) {
       request.get('http://localhost:8080/pizza', function(error, response, body) {
       	// First, assert that the status code is what it's supposed to be (exactly 404) if the listing were missing.
-        
+         response.statusCode.should.deepEqual(404);
         
         // For the last assertion, check that the string output is the same message server.js outputs when a listing is missing:
         // Finally, call "done();" to finish!
         
-
+		 body.should.deepEqual('404, Page Not Found');
+		 done();
       });
     });
   });
