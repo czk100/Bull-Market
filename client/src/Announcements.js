fbfff8f7694c3235074ea4237d79f0366ceb7c1d
colin kubisiak <ckubisiak@ufl.edu>
2021-03-23 17:23:26 -0400

    new client folder from front-end repo

---
+++ client/src/Announcements.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import "./Announcements.css";
+import React from "react";
+
+function Announcements() {
+  return <div>this is Announcements</div>;
+}
+
+export default Announcements;
---
+++ client/src/textLoader.js
@@ -0,0 +1,5 @@
+import React from "react";
+function Preload() {
+  //we need to use a fetch here
+}
+export default Preload;
