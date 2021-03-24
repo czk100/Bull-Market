54d7d068f188f5baebb3ecd110c4436d3cebf742
jhorn00 <dawshorn@gmail.com>
2021-03-24 16:52:31 -0400

    in progress

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import "./Home.css";
 import React from "react";
-import Preload from "./textLoader.js";
+import TextLoader from "./textLoader.js";
 
 function Home() {
   return (
     <div class="home-container">
       <h1 class="display-3">Home</h1>
       <img class="img_fluid" src={"./logo.png"} alt="UFSIC Logo" />
+      <div>
+        <TextLoader />
+      </div>
     </div>
   );
 }
--- client/src/textLoader.js
+++ client/src/textLoader.js
@@ -1,5 +1,34 @@
-import React from "react";
-function Preload() {
-  //we need to use a fetch here
+import React, { useState, useEffect } from "react";
+
+function TextLoader() {
+  const [data, setData] = useState([]);
+  const TextLoad = () => {
+    //we need to use a fetch here
+    fetch("./TestData.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson);
+        setData(myJson);
+      });
+  };
+
+  useEffect(() => {
+    TextLoad();
+  }, []);
+
+  return (
+    <div className="TextLoader">
+      {data && data.length > 0 && data.map((item) => <p>{item.about}</p>)}
+    </div>
+  );
 }
-export default Preload;
+
+export default TextLoader;
