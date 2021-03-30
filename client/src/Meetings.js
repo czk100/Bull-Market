8609b377ac496e178781342e93f677856f04bb92
jhorn00 <dawshorn@gmail.com>
2021-03-30 14:17:42 -0400

    formatting and calendar prep

--- client/src/Meetings.js
+++ client/src/Meetings.js
@@ -6,6 +6,9 @@ function Meetings() {
   return (
     <div className="meetings-container">
       <h1 class="display-3">Meetings</h1>
+      <div class="calendar-container">
+        <h2 class="display-5">Calendar</h2>
+      </div>
     </div>
   );
 }
