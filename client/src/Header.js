646db8949cd0236f42ea4bcba55148f7a4dcb91e
jhorn00 <dawshorn@gmail.com>
2021-04-01 20:11:29 -0400

    Announcement initial formatting and corrected header formatting

--- client/src/Header.js
+++ client/src/Header.js
@@ -5,7 +5,11 @@ import React from "react";
 function Header() {
   return (
     <div class="container">
-      <h1 class="banner">UF Student Investment Club</h1>
+      <h1 class="banner">
+        <a class="banner-link" href="/">
+          UF Student Investment Club
+        </a>
+      </h1>
       <ul class="nav nav-expand-lg nav-justified">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/">
