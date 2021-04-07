5c614e394416d608453b69591e696a72fb96c93b
colin kubisiak <ckubisiak@ufl.edu>
2021-04-07 11:46:27 -0400

    finalized faq backend

--- server/routes/api/questions.js
+++ server/routes/api/questions.js
@@ -7,24 +7,66 @@ const Question = require('../../models/Question');
 // @route GET applicaitons
 // @desc Get ALL Announcement
 // @access Public
-router.get('/', (req, res) => {
-    Question.find()
-        .sort({date : -1})
-        .then(items => res.json(items));
+router.get("/", (req, res) => {
+  Question.find()
+    .sort({ date: -1 })
+    .then((items) => res.json(items));
 });
-
 
 // @route POST api/Announcement
 // @desc Create An Announcement
 // @access Public
-router.post('/', (req, res) => {
-    const newQuestion = new Question({
-        question: req.body.question,
-        answer: req.body.answer
-    });
+router.post("/", (req, res) => {
+  const newQuestion = new Item({
+    question: req.body.question,
+    answer: req.body.answer,
+  });
 
-    newQuestion.save()
-        .then(question => res.json(question));
+  newQuestion.save().then((question) => res.json(question));
+});
+
+// @route PUT api/Announcement
+// @desc Edit An Announcement
+// @access Admin
+router.put("/", (req, res) => {
+  console.log(req.body);
+  Announcement.findOne({ question: req.body.oldQuestion }, function (err, editData) {
+    if (err) {
+      console.log(err);
+    }
+    if (!editData) {
+      res.json("no data");
+    } else {
+      editData.answer = req.body.newData.answer;
+      editData.question = req.body.newData.question;
+      editData.save(function (err) {
+        if (err) {
+          console.log(err);
+          res.json("edit save failed");
+        } else {
+          res.json("edit Complete");
+        }
+      });
+    }
+  });
+});
+
+// @route DELETE api/items/:id
+// @desc Delete A Announcement
+// @access Public
+router.delete("/", (req, res) => {
+  console.log(req.body);
+  Question.findOneAndDelete({ question: req.body.question }, function (err, docs) {
+    if (err) {
+      res.json("delete failed");
+      console.log(err);
+    } else if (docs) {
+      res.json("delete Complete");
+      console.log(docs);
+    } else {
+      res.json("docs null Complete");
+    }
+  });
 });
 
 module.exports = router;
