e7cddd45bee5aa86b3c11348f53e30d6d52b5972
colin kubisiak <ckubisiak@ufl.edu>
2021-04-07 12:31:31 -0400

    progress

---
+++ client/src/Components/FaqComponent.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from "react";
+import "./FaQComponent.css";
+const axios = require("axios");
+
+const FaqComponent = (props) => {
+  const [data, setData] = useState([]);
+  const deleteFaq = () => {
+    var toDelete = { data: { name: props.parentData.name } };
+    console.log(toDelete);
+    axios
+      .delete("http://localhost:5000/api/announcements", toDelete)
+      .then(function (response) {
+        console.log(response);
+      });
+  };
+
+  const editAnnouncement = () => {
+    var toEdit = {
+      oldName: props.parentData.name,
+      newData: data,
+    };
+    console.log(toEdit);
+    axios
+      .put("http://localhost:5000/api/announcements", toEdit)
+      .then(function (response) {
+        console.log(response);
+      });
+  };
+
+  useEffect(() => {
+    console.log(props);
+    setData(props.parentData);
+  }, []);
+
+  const handleTitleChange = (e) => {
+    var newData = {
+      name: e.target.value,
+      content: data.content,
+    };
+    setData(newData);
+  };
+
+  const handleContentChange = (e) => {
+    var newData = {
+      name: data.name,
+      content: e.target.value,
+    };
+    setData(newData);
+  };
+
+  return (
+    <div className="Announcement-Component">
+      <div class="card">
+        <div class="card-header justify-content-between">
+          <code class="timestamp">DATE PLACEHOLDER</code>
+          <div class="btn-group">
+            <button id="del" class="btn" onClick={deleteAnnouncement}>
+              Delete
+            </button>
+            <button id="ed" class="btn" onClick={editAnnouncement}>
+              Edit
+            </button>
+          </div>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">{props.parentData.name}</h5>
+          <p class="card-text">{props.parentData.content}</p>
+          <div class="textArea">
+            <input
+              type="text"
+              name="details name"
+              placeholder="Title"
+              class="form-control"
+              value={data.name}
+              onChange={(e) => handleTitleChange(e)}
+            />
+            <input
+              type="text"
+              name="details content"
+              placeholder="Content"
+              class="form-control"
+              value={data.content}
+              onChange={(e) => handleContentChange(e)}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AnnouncementComponent;
--- client/src/FAQ.js
+++ client/src/FAQ.js
@@ -1,13 +1,35 @@
 import "./App.css";
 import "./FAQ.css";
 import React from "react";
+import FaqLoader from "./Loaders/FaqLoader.js";
+import FaqAdder from "./Adders/FaqAdder.js";
 
 function FAQ() {
   return (
     <div className="faq-container">
       <h1 class="display-3">Frequently Asked Questions</h1>
+      <div className="faqTextEditable">
+        <div className="faqFormat">
+          <FaqAdder />
+        </div>
+        <div className="messageFormat">
+          <FaqLoader />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default FAQ;
+/////////////////////////////////////////////////////
+
+function Announcements() {
+  return (
+    <div class="announcements-container">
+      <h1 class="display-3">Announcements</h1>
+      
+    </div>
+  );
+}
+
+export default Announcements;
