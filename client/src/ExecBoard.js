61537f9056e6cac81821d137d567057521b41af0
steven <stevenrosenthal@ufl.edu>
2021-04-16 11:42:24 -0400

    add front end looks pretty good

--- client/src/ExecBoard.js
+++ client/src/ExecBoard.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import "./ExecBoard.css";
 import React from "react";
+// import ExecLoader from "./Loaders/ExecLoader";
+import ExecAdder from "./Adders/ExecAdder";
 
 function ExecBoard() {
   return (
-    <div className="execboard-container">
+    <div className="exec-container">
       <h1 class="display-3">Executive Board</h1>
+      <div className="execTextEditable">
+        {/* <div className="messageFormat">
+          <ExecLoader />
+        </div> */}
+        <div className="execFormat">
+          <ExecAdder />
+        </div>
+      </div>
     </div>
   );
 }
