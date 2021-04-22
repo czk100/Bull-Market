c83bb95380d1f56033f81bda043a1db63aaa21b4
colin kubisiak <ckubisiak@ufl.edu>
2021-04-21 22:54:39 -0400

    added calendar

--- client/src/Meetings.js
+++ client/src/Meetings.js
@@ -43,6 +43,7 @@ const Meetings = (props) => {
       <div class="calendar-container">
         <h2 class="display-5">Calendar</h2>
         <iframe
+          title="calendar-google"
           src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=dWZsb3JpZGFzaWNAZ21haWwuY29t&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23039BE5&amp;color=%2333B679"
           styles="border:solid 1px #777"
           width="800"
